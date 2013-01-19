50e4e85b3221f7e958b1dd55093f18b6647659c8
entertainment <entertainment@m.dlink>
2013-01-19 22:43:15 +0400

    Initial Commit

---
+++ Blockout/AAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AAppDelegate.m
+//  Blockout
+//
+//  Created by entertainment on 19.01.13.
+//  Copyright (c) 2013 Landsknecht. All rights reserved.
+//
+
+#import "AAppDelegate.h"
+
+@implementation AAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Blockout/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Blockout
+//
+//  Created by entertainment on 19.01.13.
+//  Copyright (c) 2013 Landsknecht. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
