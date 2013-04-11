88628a32c03154c99bcd3b63f0c2291ca4c1cc17
admin <admin@mterekhov.local>
2013-04-11 22:35:07 +0400

    deleting timer classes

--- Blockout/ATimer.mm
+++
@@ -1,9 +0,0 @@
-#include "AMacTimer.h"
-
-//==============================================================================
-
-ATimer* ATimer::createTimer(const float interval, AFireObject* target, void* usersData, const bool repeats)
-{
-    AMacTimer* newTimer = new AMacTimer(interval, target, usersData, repeats);
-    return newTimer;
-}
